98f14c5fb2a2d9f048f52ea037dabc5b08467775
Frank Hartling <feduardohg@gmail.com>
2022-12-21 02:01:55 -0400

    Update AndroidManifest.xml

---
+++ Taskist/build/app/generated/source/buildConfig/debug/com/huextrat/taskist/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.huextrat.taskist;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.huextrat.taskist";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "1.0.0";
+}
